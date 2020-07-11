3591660fa13c60cd087287291d829fe9bbbb8aa9
Florian Marending <florian@marending.dev>
2020-07-12 00:24:13 +0200

    Indent according to PEP8

--- server/main.py
+++ server/main.py
@@ -9,29 +9,34 @@ CORS(app)
 
 NOTES_DIR = '/home/florian/Notes'
 
+
 @app.route('/files', methods=['GET'])
 def listfiles():
-  files = get_file_list()
-  return json.dumps(files)
+    files = get_file_list()
+    return json.dumps(files)
+
 
 @app.route('/files', methods=['POST'])
 def createfile(file):
-  content = request.data.decode("utf-8")
-  f = open(os.path.join(NOTES_DIR, file), 'w')
-  f.write(content)
-  f.close()
-  return 'writing ' + file
+    content = request.data.decode("utf-8")
+    f = open(os.path.join(NOTES_DIR, file), 'w')
+    f.write(content)
+    f.close()
+    return 'writing ' + file
+
 
 @app.route('/files/<id>', methods=['GET'])
 def readfile(id):
-  f = open(os.path.join(NOTES_DIR, id), 'r')
-  return f.read()
+    f = open(os.path.join(NOTES_DIR, id), 'r')
+    return f.read()
+
 
 @app.route('/files/<id>', methods=['PUT'])
 def updatefile(id):
-  # Fail if file exists (guards against concurrent access errors)
-  pass
+    # Fail if file exists (guards against concurrent access errors)
+    pass
+
 
 @app.route('/files/<id>', methods=['DELETE'])
 def deletefile(id):
-  pass
+    pass
--- server/util.py
+++ server/util.py
@@ -1,17 +1,18 @@
 import os
 from main import NOTES_DIR
 
+
 def get_file_list():
-  filenames = []
+    filenames = []
 
-  for root, dirs, files in os.walk(NOTES_DIR):
-    for d in dirs:
-      fullpath = os.path.join(root, d)
-      partialpath = fullpath[len(NOTES_DIR) + 1:]
-      filenames.append(partialpath)
-    for f in files:
-      fullpath = os.path.join(root, f)
-      partialpath = fullpath[len(NOTES_DIR) + 1:]
-      filenames.append(partialpath)
+    for root, dirs, files in os.walk(NOTES_DIR):
+        for d in dirs:
+            fullpath = os.path.join(root, d)
+            partialpath = fullpath[len(NOTES_DIR) + 1:]
+            filenames.append(partialpath)
+        for f in files:
+            fullpath = os.path.join(root, f)
+            partialpath = fullpath[len(NOTES_DIR) + 1:]
+            filenames.append(partialpath)
 
-  return sorted(filenames)
+    return sorted(filenames)
